2a600bc894609ca55a1b000598c595ecda762d2e
naresh <nareshnrnds@gmail.com>
2024-09-10 14:52:55 +0530

    updating  python practced files

---
+++ .ipynb_checkpoints/first-checkpoint.py
@@ -0,0 +1,14 @@
+a =1000
+b = 2000
+c = a+b
+print(c)
+print("id of a:",id(a),"type of a" , type(a))
+print("id of b:" ,id(b),"type of b" , type(b))
+print("id of c:",id(c),"type of c" , type(c))
+a =1000
+b=a
+print(id(a))
+print(id(b))
+
+a =3000
+print(b)
